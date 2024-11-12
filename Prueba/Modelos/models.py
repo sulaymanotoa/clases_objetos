f04aef580c9fc512c7b241e6673806c51883abde
sulaymanotoa <sulay.manotoa7233@utc.edu.ec>
2024-11-12 11:04:40 -0500

    tarea en clases

--- Prueba/Modelos/models.py
+++ Prueba/Modelos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Estudiantes( models.Model):
+    cedula.models.charFile(primary_key=)(max_length=10)
+    nombre = models.charField(max_length=30,blank=False)
+    apeliido = models.textField(max_length=30, blank=False)
+    dierecccion = models.CharField(max_length=30,blank=False)
+    telefono = models.CharField(max_length=30,blank=False)
+
+    def _str_(self):
+        return self.nombre
+    class Docente(models.Model)
+    idDocente =. models.charFile(primary_key=)(max_length=10)
+    nombre = models.charField(max_length=30,blank=False)
+    apeliido = models.textField(max_length=30, blank=False)
+
+    def _str_(self):
+        class calificacion (models.Model):
+            estudiante=models.ForeignKey(estudiantes,ondelete=Model)
+         
+
+        Estudiantes=models.ForeignKey(on_delete=)
+    
+     
+
